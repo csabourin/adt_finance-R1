f6b32f72b992e5ba6207b29399ec253c1400bb05
Segalex <96079629+Segalex@users.noreply.github.com>
2022-06-13 10:31:19 -0400

    Émettre fixed in french

--- components/slides/plan/adjust_workplan.vue
+++ components/slides/plan/adjust_workplan.vue
@@ -261,7 +261,7 @@
     "pRes2": "<ul><li>1 agent</li><li>2 analystes</li><li>1 agent subalterne</li></ul>",
     "pA3": "Renouveler les permis de p&ecirc;che commerciale",
     "pSA3": "<ul><li> Examiner les dossiers de renouvellement des demandeurs</li><li> Délivrer des renouvellements de permis&nbsp;</li></li></ul>",
-    "pD3": "<ul><li> Délivrer les renouvellements de permis dans les 30 jours suivant la demande</li><li> Délivrer 300  renouvellements de permis par an</li></ul>",
+    "pD3": "<ul><li> Émettre les renouvellements de permis dans les 30 jours suivant la demande</li><li> Émettre 300  renouvellements de permis par an</li></ul>",
     "pRes3": "<ul><li>1 agent</li><li>1 analyste subalterne</li></ul>",
     "q1":
     {
